9430b290c8f72eca2e6ef1aa220b3f5c7ceff319
AndrzejOp <andrzejopon3@gmail.com>
2024-12-18 18:27:05 +0100

    Small cosmetics and added keywords dictionary for injuries

---
+++ injuries_keywords_dictionary.py
@@ -0,0 +1,79 @@
+critical_phrases = {
+    "head": [
+        {"phrase": "utrata przytomnoœci", "threat_level": 1},
+        {"phrase": "krwiak", "threat_level": 1},
+        {"phrase": "z³amanie", "threat_level": 1},
+        {"phrase": "krwawienie z uszu", "threat_level": 1},
+        {"phrase": "rozleg³y obrzêk", "threat_level": 0},
+        {"phrase": "przyt³umienie œwiadomoœci", "threat_level": 1},
+        {"phrase": "utrata œwiadomoœci", "threat_level": 1}
+    ],
+    "neck": [
+        {"phrase": "obrzêk", "threat_level": 0},
+        {"phrase": "uraz", "threat_level": 0},
+        {"phrase": "odma podskórna", "threat_level": 1},
+        {"phrase": "ból karku", "threat_level": 0}
+    ],
+    "chest": [
+        {"phrase": "zatrzymanie oddechu", "threat_level": 1},
+        {"phrase": "odma prê¿na", "threat_level": 1},
+        {"phrase": "bóle", "threat_level": 0},
+        {"phrase": "dusznoœæ narastaj¹ca", "threat_level": 1},
+        {"phrase": "z³amanie ¿eber", "threat_level": 0},
+        {"phrase": "krwiak op³ucnej", "threat_level": 1},
+        {"phrase": "zawa³ serca", "threat_level": 1},
+        {"phrase": "dusznoœæ nag³a", "threat_level": 1},
+        {"phrase": "sinica", "threat_level": 1},
+        {"phrase": "krwiaki", "threat_level": 1},
+        {"phrase": "krwiak", "threat_level": 1}
+    ],
+    "abdomen": [
+        {"phrase": "brzuch deskowaty", "threat_level": 1},
+        {"phrase": "krwawienie z przewodu pokarmowego", "threat_level": 1},
+        {"phrase": "uraz têpy brzucha", "threat_level": 1},
+        {"phrase": "ból rozlany", "threat_level": 0},
+        {"phrase": "wstrz¹s krwotoczny", "threat_level": 1},
+        {"phrase": "krwiak", "threat_level": 1},
+        {"phrase": "odbarwienie skóry wokó³ pêpka", "threat_level": 0},
+        {"phrase": "utrata apetytu z gor¹czk¹", "threat_level": 0},
+        {"phrase": "ból promieniuj¹cy do pleców", "threat_level": 0}
+    ],
+    "pelvis": [
+        {"phrase": "z³amanie", "threat_level": 0},
+        {"phrase": "krwawienie z pochwy", "threat_level": 0},
+        {"phrase": "brak czucia w koñczynach dolnych", "threat_level": 1},
+        {"phrase": "bóle", "threat_level": 0},
+        {"phrase": "krwawienie z cewki moczowej", "threat_level": 0},
+        {"phrase": "utrata funkcji zwieraczy", "threat_level": 1}
+    ],
+    "thigh": [
+        {"phrase": "zatrzymanie kr¹¿enia", "threat_level": 1},
+        {"phrase": "z³amanie otwarte", "threat_level": 0},
+        {"phrase": "utrata ruchomoœci koñczyny dolnej", "threat_level": 0},
+        {"phrase": "brak têtna obwodowego", "threat_level": 1},
+        {"phrase": "sinica", "threat_level": 1},
+        {"phrase": "masywny obrzêk", "threat_level": 0}
+    ],
+    "lower_leg": [
+        {"phrase": "z³amanie otwarte", "threat_level": 0},
+        {"phrase": "utrata ruchomoœci", "threat_level": 0},
+        {"phrase": "sinica", "threat_level": 1},
+        {"phrase": "masywny obrzêk", "threat_level": 0},
+        {"phrase": "ostra deformacja", "threat_level": 0}
+    ],
+    "higher_hand": [
+        {"phrase": "z³amanie otwarte", "threat_level": 0},
+        {"phrase": "utrata ruchomoœci", "threat_level": 0},
+        {"phrase": "brak têtna obwodowego", "threat_level": 1},
+        {"phrase": "zranienie z g³êbokim uszkodzeniem", "threat_level": 1},
+        {"phrase": "sinica", "threat_level": 1},
+        {"phrase": "obrzêk masywny", "threat_level": 0}
+    ],
+    "lower_hand": [
+        {"phrase": "z³amanie otwarte", "threat_level": 0},
+        {"phrase": "utrata ruchomoœci", "threat_level": 0},
+        {"phrase": "sinica", "threat_level": 1},
+        {"phrase": "masywny obrzêk", "threat_level": 0},
+        {"phrase": "ostra deformacja", "threat_level": 0}
+    ]
+}
